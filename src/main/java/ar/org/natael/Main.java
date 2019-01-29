7574dee8d9acc2eb5543335280b6b30af6bc7653
Rafael Barrera Oro <rbarrera@medallia.com>
2019-01-29 01:17:42 -0300

    aded loading of guests from xlsx file

--- src/main/java/ar/org/natael/Main.java
+++ src/main/java/ar/org/natael/Main.java
@@ -5,8 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -14,34 +16,43 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 class Main {
 
+	private static final int FIRST_NAME_COLUMN_INDEX = 0;
+	private static final int LAST_NAME_COLUMN_INDEX = 1;
+	private static final int EMAIL_COLUMN_INDEX = 2;
+
 	public static void main(String[] args) {
-		
-		if(args.length == 0) {
+
+		if (args.length == 0) {
 			System.out.println("Uso: java -jar natael.jar <archivo>");
+		} else {
+			new Main().process(args[0]);
 		}
-		else {
-			new Main().process(args[0]);;
+	}
+
+	private List<Guest> loadGuests(InputStream inputStream) throws IOException {
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		List<Guest> guests = new ArrayList<Guest>();
+		Sheet sheet = workbook.getSheetAt(0);
+		for (int rowIndex = 1; rowIndex < sheet.getPhysicalNumberOfRows() - 1; rowIndex++) {
+			Row row = sheet.getRow(rowIndex);
+			guests.add(new Guest.Builder().firstName(row.getCell(FIRST_NAME_COLUMN_INDEX).getStringCellValue())
+					.lastName(row.getCell(LAST_NAME_COLUMN_INDEX).getStringCellValue())
+					.email(row.getCell(EMAIL_COLUMN_INDEX).getStringCellValue())
+					.build());
 		}
-	}	
-	
+		return guests;
+	}
+
 	public void process(String path) {
 		try (InputStream inputStream = new FileInputStream(new File(path))) {
-			
-			Workbook workbook = new XSSFWorkbook(inputStream);
-			Sheet sheet = workbook.getSheetAt(0);
-			for(int rowIndex = 0; rowIndex < sheet.getPhysicalNumberOfRows(); rowIndex++) {
-				Row row = sheet.getRow(rowIndex);
-				for(int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
-					Cell cell = row.getCell(cellIndex);
-					System.out.println(cell.getStringCellValue());
-				}
-				System.out.println("-----");
-			}
- 
+			List<Guest> guests = loadGuests(inputStream);
+			guests.stream()
+				.filter(guest -> guest.getLastName().isEmpty() || guest.getFirstName().isEmpty() || guest.getFirstName().toLowerCase().contains("pareja")  || guest.getLastName().toLowerCase().contains("pareja")  || guest.getFirstName().toLowerCase().contains("esposa")  || guest.getLastName().toLowerCase().contains("esposa"))
+				.forEach(System.out::println);
 		} catch (FileNotFoundException e) {
 			System.out.println("No se pudo encontrar el archivo: " + path);
 		} catch (IOException e) {
 			System.out.println("No se pudo acceder al archivo: " + path);
-		}		
+		}
 	}
 }
