382dd956b46c5fb87b9c66ef5e0a85ee8b792334
Rafael Barrera Oro <rbarrera@medallia.com>
2019-04-08 10:47:23 -0300

    simplified

--- src/main/java/ar/org/natael/Guest.java
+++ src/main/java/ar/org/natael/Guest.java
@@ -2,44 +2,12 @@ package ar.org.natael;
 
 public class Guest {
 	
-	public static enum STATE {
-		PENDIENTE,
-		CONFIRMADO,
-		CANCELADO
-	}
-
 	private String firstName;
 	private String lastName;
-	private String email;
-	private String phone;
-	private String cell;
-	private String group;
-	private STATE state;
-	private String confirmed;
-	private String menu;
-	private String address;
-	private String postalCode;
-	private String city;
-	private String province;
-	private String table;
-	private String sex;
 	
 	private Guest(Builder builder) {
 		this.firstName = builder.firstName;
 		this.lastName = builder.lastName;
-		this.email = builder.email;
-		this.phone = builder.phone;
-		this.cell = builder.cell;
-		this.group = builder.group;
-		this.state = builder.state;
-		this.confirmed = builder.confirmed;
-		this.menu = builder.menu;
-		this.address = builder.address;
-		this.postalCode = builder.postalCode;
-		this.city = builder.city;
-		this.province = builder.province;
-		this.table = builder.table;
-		this.sex = builder.sex;
 	}
 
 	public String getFirstName() {
@@ -48,58 +16,6 @@ public class Guest {
 
 	public String getLastName() {
 		return lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getPhone() {
-		return phone;
-	}
-
-	public String getCell() {
-		return cell;
-	}
-
-	public String getGroup() {
-		return group;
-	}
-
-	public STATE getState() {
-		return state;
-	}
-
-	public String getConfirmed() {
-		return confirmed;
-	}
-
-	public String getMenu() {
-		return menu;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public String getProvince() {
-		return province;
-	}
-
-	public String getTable() {
-		return table;
-	}
-
-	public String getSex() {
-		return sex;
 	}
 	
 	@Override
@@ -110,19 +26,6 @@ public class Guest {
 	public static class Builder {
 		private String firstName;
 		private String lastName;
-		private String email;
-		private String phone;
-		private String cell;
-		private String group;
-		private STATE state;
-		private String confirmed;
-		private String menu;
-		private String address;
-		private String postalCode;
-		private String city;
-		private String province;
-		private String table;
-		private String sex;
 		
 		public Builder() {}
 		
@@ -135,72 +38,6 @@ public class Guest {
 			this.lastName = lastName;
 			return this;
 		}
-		
-		public Builder email(String email) {
-			this.email = email;
-			return this;
-		}
-		
-		public Builder phone(String phone) {
-			this.phone = phone;
-			return this;
-		}
-		
-		public Builder cell(String cell) {
-			this.cell = cell;
-			return this;
-		}
-		
-		public Builder group(String group) {
-			this.group = group;
-			return this;
-		}
-		
-		public Builder state(STATE state) {
-			this.state = state;
-			return this;
-		}
-		
-		public Builder confirmed(String confirmed) {
-			this.confirmed = confirmed;
-			return this;
-		}
-		
-		public Builder menu(String menu) {
-			this.menu = menu;
-			return this;
-		}
-		
-		public Builder address(String address) {
-			this.address = address;
-			return this;
-		}
-		
-		public Builder postalCode(String postalCode) {
-			this.postalCode = postalCode;
-			return this;
-		}
-		
-		public Builder city(String city) {
-			this.city = city;
-			return this;
-		}
-		
-		public Builder province(String province) {
-			this.province = province;
-			return this;
-		}
-		
-		public Builder table(String table) {
-			this.table = table;
-			return this;
-		}
-		
-		public Builder sex(String sex) {
-			this.sex = sex;
-			return this;
-		}
-		
 		public Guest build() {
 			return new Guest(this);
 		}
--- src/main/java/ar/org/natael/Main.java
+++ src/main/java/ar/org/natael/Main.java
@@ -1,62 +1,30 @@
 package ar.org.natael;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 class Main {
 
-	private static final int FIRST_NAME_COLUMN_INDEX = 0;
-	private static final int LAST_NAME_COLUMN_INDEX = 1;
-	private static final int EMAIL_COLUMN_INDEX = 2;
-	private static final int STATE_COLUMN_INDEX = 7;
+	private static final int LAST_NAME_COLUMN_INDEX = 0;
+	private static final int FIRST_NAME_COLUMN_INDEX = 1;
 
-	public static void main(String[] args) {
-
-		if (args.length == 0) {
-			System.out.println("Uso: java -jar natael.jar <archivo>");
-		} else {
-			new Main().process(args[0]);
-		}
-	}
-
-	private List<Guest> loadGuests(InputStream inputStream) throws IOException {
-		Workbook workbook = new XSSFWorkbook(inputStream);
+	public static void main(String[] args) throws Exception {
+		File file = new File("/Users/rbarrera/repos/natael/invitados.xlsx");
+		
+		org.apache.poi.ss.usermodel.Workbook workbook = WorkbookFactory.create(file);
 		List<Guest> guests = new ArrayList<Guest>();
 		Sheet sheet = workbook.getSheetAt(0);
 		for (int rowIndex = 1; rowIndex < sheet.getPhysicalNumberOfRows() - 1; rowIndex++) {
 			Row row = sheet.getRow(rowIndex);
 			guests.add(new Guest.Builder()
 					.firstName(row.getCell(FIRST_NAME_COLUMN_INDEX).getStringCellValue())
-					.lastName(row.getCell(LAST_NAME_COLUMN_INDEX).getStringCellValue())
-					.email(row.getCell(EMAIL_COLUMN_INDEX).getStringCellValue())
-					.state(Guest.STATE.valueOf(row.getCell(STATE_COLUMN_INDEX).getStringCellValue()))
-					.build());
-		}
-		return guests;
-	}
-
-	public void process(String path) {
-		try (InputStream inputStream = new FileInputStream(new File(path))) {
-			List<Guest> guests = loadGuests(inputStream);
-			guests.stream()
-				.filter(guest -> Guest.STATE.PENDIENTE.equals(guest.getState()))
-				.map(Guest::toString)
-				.sorted()
-				.forEach(System.out::println);
-		} catch (FileNotFoundException e) {
-			System.out.println("No se pudo encontrar el archivo: " + path);
-		} catch (IOException e) {
-			System.out.println("No se pudo acceder al archivo: " + path);
+					.lastName(row.getCell(LAST_NAME_COLUMN_INDEX).getStringCellValue()).build());
+			System.out.println(row.getCell(2).getNumericCellValue());
 		}
 	}
 }
